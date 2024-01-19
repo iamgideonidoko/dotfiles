49a4c932b2febceffc386f1489cff5d64b7562cb
Gideon Idoko <iamgideonidoko@gmail.com>
2024-01-19 05:34:47 +0100

    feat: configure custom mappins for lsp

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -1,7 +1,8 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'github_dark' }
-M.plugins = 'custom.plugins' -- Tells NvChad where we've defined any custom configuration
+M.ui = { theme = "github_dark" }
+M.plugins = "custom.plugins" -- Tells NvChad where custom plugin config is located
+M.mappings = require "custom.mappings" -- Tells NvChad where custom mappings config is located 
 
 return M
--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -3,7 +3,6 @@
 local api = vim.api
 local opt = vim.opt
 local cmd = vim.cmd
-local keymap = vim.keymap
 
 -- Enable relative line numbering
 api.nvim_exec([[
@@ -16,11 +15,3 @@ opt.foldexpr = "nvim_treesitter#foldexpr()"
 cmd([[ set nofoldenable ]])
 opt.foldcolumn = "1"
 
--- Bindings for Lspsaga
-local lspsaga_binding_opt = { noremap = true, silent = true }
-keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', lspsaga_binding_opt)
-
--- Binding to run http request under the cursor
-api.nvim_set_keymap('n', '<leader>rr', '<Plug>RestNvim', {})
--- Binding to preview http request to be made with cURL
-api.nvim_set_keymap('n', '<leader>rp', '<Plug>RestNvimPreview', {})
