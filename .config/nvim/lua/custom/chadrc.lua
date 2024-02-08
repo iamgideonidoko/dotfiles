02d2a3933e871281cedda8e92a23fd0e9029b272
Gideon Idoko <iamgideonidoko@gmail.com>
2024-02-08 05:38:09 +0100

    feat: enable transparency

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -1,7 +1,7 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = "github_dark" }
+M.ui = { theme = "github_dark", transparency = true }
 M.plugins = "custom.plugins" -- Tells NvChad where custom plugin config is located
 M.mappings = require "custom.mappings" -- Tells NvChad where custom mappings config is located 
 
