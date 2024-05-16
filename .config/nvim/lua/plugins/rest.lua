7467f795a86ace937aaa654ee3c3d4cd252c84d5
Gideon Idoko <iamgideonidoko@gmail.com>
2024-05-16 18:16:19 +0100

    feat(nvim:rest): add bindings for easily calling rest requests

--- .config/nvim/lua/plugins/rest.lua
+++ .config/nvim/lua/plugins/rest.lua
@@ -10,6 +10,8 @@ return {
 			require("rest-nvim").setup({
 				-- Get config options at: https://github.com/rest-nvim/rest.nvim/tree/84e81a19ab24ccf05c9233d34d4dfce61c233abe?tab=readme-ov-file#packernvim
 			})
+			vim.keymap.set("n", "<leader>rr", "<Plug>RestNvim", { desc = "[R]request [r]run" })
+			vim.keymap.set("n", "<leader>rp", "<Plug>RestNvimPreview", { desc = "[R]equest [p]preview" })
 		end,
 	},
 }
