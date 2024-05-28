b2c847ca282e2abf2ff949a92ee8f6da57a3af85
Gideon Idoko <iamgideonidoko@gmail.com>
2024-05-28 04:43:34 +0100

    feat(nvim): move cursor from end or start of line to next or prev

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,65 +1,72 @@
 -- [[Setting options (see `:help vim.opt` & `:help option-list`)]]
 
-vim.opt.relativenumber = true
-vim.opt.number = true
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = "a"
+opt.mouse = "a"
 
 -- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
+opt.showmode = false
 
 -- Sync clipboard between OS and Neovim instead of being independent (see:help 'clipboard'`)
-vim.opt.clipboard = "unnamedplus"
+opt.clipboard = "unnamedplus"
 
 -- Indenting
 -- vim.opt.breakindent = true
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.smartindent = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.smartindent = true
+opt.tabstop = 2
+opt.softtabstop = 2
 
 -- Set fold based on treesitter expression
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.cmd([[ set nofoldenable ]])
-vim.opt.foldcolumn = "1"
+opt.foldcolumn = "1"
 
 -- Save undo history
-vim.opt.undofile = true
+opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = "yes"
+opt.signcolumn = "yes"
 
 -- Decrease update time
-vim.opt.updatetime = 250
+opt.updatetime = 250
 
 -- Decrease mapped sequence wait time (smaller sequence displays which-key popup sooner)
-vim.opt.timeoutlen = 300
+opt.timeoutlen = 300
 
 -- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+opt.splitright = true
+opt.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor (see `:help 'list'` & `:help 'listchars'`)
-vim.opt.list = false
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+opt.list = false
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = "split"
+opt.inccommand = "split"
 
 -- Show which line your cursor is on
-vim.opt.cursorline = true
+opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+opt.scrolloff = 10
 
--- Disable Neovim startup screen (see :help shortmess)
-vim.cmd([[ set shortmess=I ]])
+-- disable nvim intro
+opt.shortmess:append "sI"
+
+-- go to previous/next line with h,l,left arrow and right arrow
+-- when cursor reaches end/beginning of line
+opt.whichwrap:append "<>[]hl"
+
 
 -- vim: ts=2 sts=2 sw=2 et
