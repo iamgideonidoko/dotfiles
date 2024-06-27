93f2fbabc36fde77c452544e51a0918797db811f
Gideon Idoko <iamgideonidoko@gmail.com>
2024-06-27 04:31:25 +0100

    feat(nvim): remap tmux nav key in oil.nvim buffer

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -14,6 +14,16 @@ return {
 				view_options = {
 					show_hidden = true, -- Files that start with "."
 				},
+				keymaps = {
+					["<C-h>"] = false,
+					["<C-l>"] = false,
+					["<C-r>"] = "actions.refresh",
+					["<C-S-s>"] = {
+						"actions.select",
+						opts = { horizontal = true },
+						desc = "Open the entry in a horizontal split",
+					},
+				},
 			})
 			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 		end,
