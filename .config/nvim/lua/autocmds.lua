71e5ed311e7d369e73155e214ca27d75d568ec36
Gideon Idoko <iamgideonidoko@gmail.com>
2024-07-02 16:13:33 +0100

    feat(nvim): safely  remove current buffer if associated file is no longer available [e211]

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -31,4 +31,34 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 	end,
 })
 
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = vim.api.nvim_create_augroup("delete_e211_buffers", { clear = true }),
+	callback = function()
+		-- Get the file path of the current buffer
+		local path = vim.api.nvim_buf_get_name(0)
+		-- Check if the path is empty
+		if path == "" then
+			-- The buffer does not have a file path
+			return
+		end
+
+		-- Check if the file path exists
+		local stat = vim.loop.fs_stat(path)
+		if stat then
+			-- The current buffer file path is valid
+			return
+		end
+		local cwd = vim.fn.getcwd()
+		-- Ensure that the file path is normalized and absolute
+		local normalized_path = vim.fn.fnamemodify(path, ":p")
+		-- print("path: ", path, "normalized path: ", normalized_path)
+		-- Check if the file path starts with the current working directory
+		if normalized_path:sub(1, #cwd) ~= cwd then
+			return
+		end
+		-- The current buffer file path is not valid so delete buffer
+		U.smart_close_buffer(true)
+	end,
+})
+
 -- vim: ts=2 sts=2 sw=2 et
