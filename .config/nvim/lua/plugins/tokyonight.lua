211643575af35ad39b99011753128c1389c6c39e
Gideon Idoko <iamgideonidoko@gmail.com>
2025-01-03 07:55:03 +0100

    feat(nvim): make color scheme transparent

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -1,7 +1,15 @@
 return { -- Colorscheme
 	"folke/tokyonight.nvim",
-	priority = 1000, -- Make sure to load this before all the other start plugins.
+	priority = 1000, -- Load before other start plugins
 	init = function()
+		---@diagnostic disable: missing-fields
+		require("tokyonight").setup({
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		})
 		vim.cmd.colorscheme("tokyonight-night")
 		vim.cmd.hi("Comment gui=none")
 	end,
