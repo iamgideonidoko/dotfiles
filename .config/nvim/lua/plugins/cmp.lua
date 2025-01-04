a5adfc525231667a31aeb65b05ee940d107fba7a
Gideon Idoko <iamgideonidoko@gmail.com>
2025-01-04 16:33:35 +0100

    refactor(nvim): doc aborting a completion

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -59,15 +59,17 @@ return { -- Autocompletion
 				["<CR>"] = cmp.mapping.confirm({ select = true }),
 				["<Tab>"] = cmp.mapping.select_next_item(),
 				["<S-Tab>"] = cmp.mapping.select_prev_item(),
-				-- Manually trigger a completion from nvim-cmp.
+				-- Manually trigger completion
 				["<C-Space>"] = cmp.mapping.complete({}),
-				-- <c-p> will move you to the right of each of the expansion locations.
-				-- <c-n> is similar, except moving you backwards.
+				-- Manually abort completion
+				["<C-e>"] = cmp.mapping.abort(),
+				-- Move to the left of each of the expansion locations
 				["<C-n>"] = cmp.mapping(function()
 					if luasnip.expand_or_locally_jumpable() then
 						luasnip.expand_or_jump()
 					end
 				end, { "i", "s" }),
+				-- Move to the right of each of the expansion locations
 				["<C-p>"] = cmp.mapping(function()
 					if luasnip.locally_jumpable(-1) then
 						luasnip.jump(-1)
