22ec103ebea61de72e540d800f8ff873ef6c1ec5
Gideon Idoko <iamgideonidoko@gmail.com>
2025-01-24 11:43:16 +0100

    chore: remove features now supported by loft

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -29,59 +29,6 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 	end,
 })
 
--- Delete buffers that return E211 i.e file no longer available
-vim.api.nvim_create_autocmd("BufEnter", {
-	group = vim.api.nvim_create_augroup("delete_e211_buffers", { clear = true }),
-	callback = function()
-		local file_path = vim.api.nvim_buf_get_name(0)
-		if file_path == "" then
-			return
-		end
-		-- Check if the file path exists
-		local stat = vim.loop.fs_stat(file_path)
-		if stat then
-			-- The current buffer file path is valid
-			return
-		end
-		local cwd = vim.fn.getcwd()
-		-- Ensure that the file path is normalized and absolute
-		local normalized_path = vim.fn.fnamemodify(file_path, ":p")
-		-- Check if the file path starts with the current working directory
-		if normalized_path:sub(1, #cwd) ~= cwd then
-			return
-		end
-		-- Make exceptions for RestNvim buffer
-		if string.find(normalized_path, "rest_nvim") then
-			return
-		end
-		-- The current buffer file path is not valid so delete buffer
-		-- close_buffer(true)
-	end,
-})
-
--- Immediately close buffer of deleted file via oil.nvim
-vim.api.nvim_create_autocmd("User", {
-	pattern = "OilActionsPost",
-	callback = function(args)
-		local parse_url = function(url)
-			return url:match("^.*://(.*)$")
-		end
-		if args.data.err then
-			return
-		end
-		for _, action in ipairs(args.data.actions) do
-			if action.type == "delete" and action.entry_type == "file" then
-				local path = parse_url(action.url)
-				local bufnr = vim.fn.bufnr(path)
-				if bufnr == -1 then
-					return
-				end
-				-- close_buffer(true, bufnr)
-			end
-		end
-	end,
-})
-
 -- Refresh status line on when recording starts and ends
 vim.api.nvim_create_autocmd({ "RecordingEnter", "RecordingLeave" }, {
 	callback = function()
