d3eeebb2524ca0433eef4501c8b0ffebf340d55d
Gideon Idoko <iamgideonidoko@gmail.com>
2025-02-24 10:45:04 +0100

    feat(hammerspoon): spin up first emulator is no running emulator to focus during openning

--- hammerspoon/modules/emulator.lua
+++ hammerspoon/modules/emulator.lua
@@ -1,11 +1,31 @@
-function focusAndroidEmulator()
+local spinUpFirstAndroidEmulator = function()
+  local emulatorPath = "/Users/" .. os.getenv("USER") .. "/Library/Android/sdk/emulator/emulator"
+  -- Get the first available AVD
+  local handle = io.popen(emulatorPath .. " -list-avds", "r")
+  if not handle then
+    hs.alert.show("Failed to list emulators")
+    return
+  end
+  local avdList = handle:read("*a")
+  handle:close()
+  -- Extract the first AVD name
+  ---@type string|nil
+  local firstAVD = avdList:match("([^\n]+)")
+  if firstAVD then
+    hs.task.new(emulatorPath, nil, { "-avd", firstAVD }):start()
+  else
+    hs.alert.show("No emulators found!")
+  end
+end
+
+local openAndroidEmulator = function()
   local win = hs.window.filter.new("qemu-system-aarch64"):getWindows()
   if win and #win > 0 then
     win[1]:focus() -- Bring the first matching window to the front
   else
-    hs.alert.show("Android Emulator is not running")
+    spinUpFirstAndroidEmulator()
   end
 end
 
 -- Register the function to be called via URL
-hs.urlevent.bind("focusAndroidEmulator", focusAndroidEmulator)
+hs.urlevent.bind("openAndroidEmulator", openAndroidEmulator)
