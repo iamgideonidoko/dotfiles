c8146be5f9be3351b0b4651271f67223097737c2
Gideon Idoko <iamgideonidoko@gmail.com>
2025-05-18 07:22:04 +0100

    fix(nvim<lint>): make eslint_d to properly search for config

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -19,13 +19,41 @@ return {
       }
     end
     local lint = require("lint")
+    local util = require("lspconfig.util")
+    lint.linters.eslint_d = {
+      name = "eslint_d",
+      cmd = "eslint_d",
+      stdin = true,
+      args = {
+        "--format",
+        "json",
+        "--stdin",
+        "--stdin-filename",
+        function()
+          return vim.api.nvim_buf_get_name(0)
+        end,
+      },
+      stream = "stdout",
+      ignore_exitcode = true,
+      env = nil,
+      parser = require("lint.parser").from_errorformat("%f:%l:%c: %m", {
+        source = "eslint_d",
+        severity = vim.diagnostic.severity.WARN,
+      }),
+      condition = function(ctx)
+        local root =
+          util.root_pattern(".eslintrc.js", ".eslintrc.json", ".eslintrc.cjs", "package.json", ".git")(ctx.filename)
+        return root ~= nil
+      end,
+    }
     lint.linters_by_ft = linters
+
     -- `.luacheck` config is attempted to be loaded from the current to system root (use below to specify exact)
     -- if lint.linters.luacheck then
     --   lint.linters.luacheck.args = { "--config", "../../.luacheckrc" }
     -- end
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "BufReadPost", "InsertLeave", "TextChanged" }, {
       group = lint_augroup,
       callback = function()
         lint.try_lint()
