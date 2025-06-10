b8590928613f1c46914cdb60c653f46af572293e
Gideon Idoko <iamgideonidoko@gmail.com>
2025-06-10 11:34:24 +0100

    chore(nvim): update rest.nvim config

--- nvim/lua/plugins/rest.lua
+++ nvim/lua/plugins/rest.lua
@@ -1,15 +1,12 @@
-return { -- HTTP client
+return {
   "rest-nvim/rest.nvim",
-  ft = {
-    "http",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      table.insert(opts.ensure_installed, "http")
+      vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<CR>", { desc = "[R]request [r]run" })
+      vim.keymap.set("n", "<leader>rl", "<cmd>Rest logs<CR>", { desc = "[R]request [l]ogs" })
+    end,
   },
-  dependencies = { { "nvim-lua/plenary.nvim" } },
-  commit = "84e81a19ab24ccf05c9233d34d4dfce61c233abe",
-  config = function()
-    require("rest-nvim").setup({
-      -- Get config options at: https://github.com/rest-nvim/rest.nvim/tree/84e81a19ab24ccf05c9233d34d4dfce61c233abe?tab=readme-ov-file#packernvim
-    })
-    vim.keymap.set("n", "<leader>rr", "<Plug>RestNvim", { desc = "[R]request [r]run" })
-    vim.keymap.set("n", "<leader>rp", "<Plug>RestNvimPreview", { desc = "[R]equest [p]preview" })
-  end,
 }
