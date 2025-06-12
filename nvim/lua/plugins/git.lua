d5fc0f860da77ea9efe85753b3792dd3cece42a5
Gideon Idoko <iamgideonidoko@gmail.com>
2025-06-12 03:38:31 +0100

    fix(nvim): add border to gitsigns windows

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -7,6 +7,7 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     opts = {
+      preview_config = { border = "rounded" },
       signs = {
         add = { text = "+" },
         change = { text = "~" },
