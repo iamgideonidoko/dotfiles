830de898440a71c34730198723c97898413706db
Gideon Idoko <iamgideonidoko@gmail.com>
2025-06-12 04:22:04 +0100

    fix(nvim): update binding for eslint fix all

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -9,16 +9,18 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
--- Keybinding for `EslintFixAll`
+-- Keybinding for ESLint fix all
 vim.api.nvim_create_autocmd("BufReadPost", {
   group = vim.api.nvim_create_augroup("custom-buffer-eslint-fix", { clear = true }),
   callback = function()
     vim.defer_fn(function()
-      if utils.command_exists("EslintFixAll") then
+      local eslint_fix_all_command = "LspEslintFixAll"
+      local eslint_fix_all_exists = utils.command_exists(eslint_fix_all_command)
+      if eslint_fix_all_exists then
         if not utils.check_keybinding_exists("n", "<leader>fe") then
           vim.keymap.set("n", "<leader>fe", function()
-            vim.cmd("EslintFixAll")
-          end, { desc = "[F]ix [E]SLint" })
+            vim.cmd(eslint_fix_all_command)
+          end, { desc = "[F]ix [e]SLint" })
         end
       else
         if utils.check_keybinding_exists("n", "<leader>fe") then
