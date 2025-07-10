31dc4b28eeea0c3c623bf7a1a6d4d3c0d76763bc
Gideon Idoko <iamgideonidoko@gmail.com>
2025-07-10 07:44:08 +0100

    feat(nvim<statusline>): add curved edges to groups with backgrounds

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,6 +1,33 @@
 return { -- Collection of small independent packages
   "echasnovski/mini.nvim",
   config = function()
+    local combine_groups = function(groups)
+      local parts = vim.tbl_map(function(s)
+        if type(s) == "string" then
+          return s
+        end
+        if type(s) ~= "table" then
+          return ""
+        end
+        local string_arr = vim.tbl_filter(function(x)
+          return type(x) == "string" and x ~= ""
+        end, s.strings or {})
+        local str = table.concat(string_arr, " ")
+        if str:len() == 0 then
+          return ""
+        end
+        local hl = vim.api.nvim_get_hl(0, { name = s.hl })
+        vim.api.nvim_set_hl(0, s.hl .. "Edge", {
+          fg = hl.bg,
+          bg = "none",
+        })
+        if hl.bg == nil then
+          return string.format("%%#%s# %s ", s.hl, str)
+        end
+        return string.format("%%#%sEdge#%%#%s#%s%%#%sEdge#", s.hl, s.hl, str, s.hl)
+      end, groups)
+      return table.concat(parts, "")
+    end
     local statusline = require("mini.statusline")
     statusline.setup({
       use_icons = vim.g.have_nerd_font,
@@ -21,7 +48,7 @@ return { -- Collection of small independent packages
           local tab_idx = require("utils").get_tab_index()
           local total_tabs = #vim.api.nvim_list_tabpages()
           local tab_indicator = total_tabs > 1 and "T" .. tab_idx or ""
-          return statusline.combine_groups({
+          return combine_groups({
             { hl = mode_hl, strings = { mode } },
             { hl = "StatusLineTabIndicator", strings = { tab_indicator } },
             { hl = "StatusLineLoftSmartOrder", strings = { loft_ui:smart_order_indicator() } },
