57764663b9a994d2216bb319fc7f3a553dcc881c
Gideon Idoko <iamgideonidoko@gmail.com>
2025-07-11 18:29:35 +0100

    chore(nvim): remove mini.ai, its poor support for tags wears me out

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -71,12 +71,6 @@ return { -- Collection of small independent packages
       },
     })
 
-    -- Better Around/Inside textobjects
-    --  - va)  - [V]isually select [A]round [)]paren
-    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-    --  - ci'  - [C]hange [I]nside [']quote
-    require("mini.ai").setup({ n_lines = 500, silent = true })
-
     -- Add/delete/replace surroundings (brackets, quotes, etc.)
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
     -- - sd'   - [S]urround [D]elete [']quotes
