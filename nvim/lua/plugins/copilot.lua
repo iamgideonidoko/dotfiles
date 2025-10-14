9543303cc401eb638ff0e0beb9c688fd07a3fbf1
Gideon Idoko <iamgideonidoko@gmail.com>
2025-10-14 20:22:01 +0100

    refactor(nvim): correct copilot prev binding

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -13,7 +13,7 @@ return {
           accept_word = false,
           accept_line = false,
           next = "<M-'>",
-          prev = "<M-\">",
+          prev = '<M-">',
           dismiss = "<C-]>",
         },
       },
