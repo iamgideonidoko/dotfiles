f97d0df64fa62278a0123a2aa08266eb31093b3a
Gideon Idoko <iamgideonidoko@gmail.com>
2025-10-27 04:31:46 +0100

    feat(sketchybar<battery>): show always

--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -3,31 +3,42 @@
 source "$CONFIG_DIR/icons.sh"
 source "$CONFIG_DIR/colors.sh"
 
+BATT_PERCENT=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 BATTERY_INFO="$(pmset -g batt)"
 PERCENTAGE=$(echo "$BATTERY_INFO" | grep -Eo "\d+%" | cut -d% -f1)
 CHARGING=$(echo "$BATTERY_INFO" | grep 'AC Power')
 
-if [ $PERCENTAGE = "" ]; then
-  exit 0
+if [ "$PERCENTAGE" = "" ]; then
+	exit 0
 fi
 
 DRAWING=on
 COLOR=$WHITE
 case ${PERCENTAGE} in
-  9[0-9]|100) ICON=$BATTERY_100; DRAWING=off
-  ;;
-  [6-8][0-9]) ICON=$BATTERY_75; DRAWING=off
-  ;;
-  [3-5][0-9]) ICON=$BATTERY_50
-  ;;
-  [1-2][0-9]) ICON=$BATTERY_25; COLOR=$ORANGE
-  ;;
-  *) ICON=$BATTERY_0; COLOR=$RED
+9[0-9] | 100)
+	ICON=$BATTERY_100
+	COLOR=$GREEN
+	;;
+[6-8][0-9])
+	ICON=$BATTERY_75
+	COLOR=$GREEN
+	;;
+[3-5][0-9])
+	ICON=$BATTERY_50
+	COLOR=$GREEN
+	;;
+[1-2][0-9])
+	ICON=$BATTERY_25
+	COLOR=$ORANGE
+	;;
+*)
+	ICON=$BATTERY_0
+	COLOR=$RED
+	;;
 esac
 
 if [[ $CHARGING != "" ]]; then
-  ICON=$BATTERY_CHARGING
-  DRAWING=off
+	ICON=$BATTERY_CHARGING
 fi
 
-sketchybar --set $NAME drawing=$DRAWING icon="$ICON" icon.color=$COLOR
+sketchybar --set "$NAME" drawing="$DRAWING" icon="$ICON" icon.color="$COLOR" label="${BATT_PERCENT}%" label.color="$COLOR"
