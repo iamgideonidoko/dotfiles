b9bfcb1e66023ac83aec5d09d4ddb7eb650c53c2
Gideon Idoko <iamgideonidoko@gmail.com>
2026-01-30 03:53:15 +0100

    chore(nvim<avante>): remove needless deps

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -25,22 +25,5 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     "folke/snacks.nvim",
-    {
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = { insert_mode = true },
-          use_absolute_path = true,
-        },
-      },
-    },
-    {
-      "MeanderingProgrammer/render-markdown.nvim",
-      opts = { file_types = { "markdown", "Avante" } },
-      ft = { "markdown", "Avante" },
-    },
   },
 }
