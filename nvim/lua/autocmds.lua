f09019a6a09d052cf52607dd819c5d708323c01f
Gideon Idoko <iamgideonidoko@gmail.com>
2026-02-23 11:11:51 +0100

    feat(nvim): jump to last update position when opening a file

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -114,3 +114,19 @@ vim.api.nvim_create_autocmd("User", {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = vim.api.nvim_create_augroup("jump-to-last-pos", { clear = true }),
+  desc = "Return to last edit position when opening a file",
+  callback = function()
+    local exclude = { "gitcommit", "commit", "gitrebase" }
+    if vim.tbl_contains(exclude, vim.bo.filetype) then
+      return
+    end
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
